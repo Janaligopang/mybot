c685004b9df3b1a832ee6af6cb333f9bca2bf0ba
Janaligopang <113940976+Janaligopang@users.noreply.github.com>
2022-10-14 02:04:01 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,15 +54,15 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['923183098772'] //ur owner number
+global.vcardowner = ['+17025147905'] //ur owner number
 global.ownername = "ZOHAIB KHAN JS" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['923183098772']
-global.ownertag = '923183098772' //ur tag number
+global.owner = ['+17025147905']
+global.ownertag = '+17025147905' //ur tag number
 global.botname = 'Cheems Bot MD V4' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['923183098772'] //ur premium numbers
+global.premium = ['+17025147905'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
